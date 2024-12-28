9312565cc883298082321c7f8b903c83eb58f100
gamithasam <gamitha@asia.com>
2024-12-28 21:34:16 +0530

    Added Exercises design to SelectExercise Doesn't have the info button No image Only can select one Selecting doesn't make the add button visiible

--- Trainly/Views/SelectExerciseView.swift
+++ Trainly/Views/SelectExerciseView.swift
@@ -10,18 +10,79 @@ import SwiftUI
 struct SelectExerciseView: View {
     let type: String
     @StateObject var vm = TrainlyViewModal()
+    let columns = [
+        GridItem(.adaptive(minimum: 100, maximum: 200), spacing: 16)
+    ]
+    @State private var selectedIndex: ExerciseEntity?
     
     var body: some View {
         NavigationView {
-            
-            List {
-                ForEach(vm.savedExercises) { item in
-                    Text(item.name ?? "No Name")
+            GeometryReader { geometry in
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 16) {
+                        ForEach(vm.savedExercises) { item in
+                            ExerciseItem(
+                                geometry: geometry,
+                                isSelected: selectedIndex == item,
+                                item: item
+                            )
+                            .onTapGesture {
+                                if selectedIndex == item {
+                                    selectedIndex = nil
+                                } else {
+                                    selectedIndex = item
+                                }
+                            }
+                        }
+                    }
+                    .padding()
                 }
             }
             .navigationTitle(type)
             .navigationBarTitleDisplayMode(.inline)
         }
+    }
+}
+
+
+struct ExerciseItem: View {
+    let geometry: GeometryProxy
+    let isSelected: Bool
+    let item: ExerciseEntity
+    
+    var width:CGFloat {
+        return (geometry.size.width - 48) / 3
+    }
+    var height:CGFloat {
+        return 150/115*width
+    }
+    
+    var body: some View {
+        ZStack {
+            if isSelected {
+                RoundedRectangle(cornerRadius: 13.5)
+                    .strokeBorder(Color.accentColor, lineWidth: 1.5)
+                    .frame(width: width, height: height)
+            }
+            
+            ZStack(alignment: .bottom) {
+                // Add image here
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: width-6, height: height-6)
+                    
+                ZStack {
+                    Rectangle()
+                        .fill(.regularMaterial)
+                        .frame(width: width-6, height: 42/115*width)
+                    
+                    Text(item.name ?? "No Name")
+                        .font(.caption)
+                }
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
+        .frame(width: width, height: height)
+        
     }
 }
 
