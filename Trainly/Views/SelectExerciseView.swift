1abbd07aee2f57098be0c142174ccced15a779f4
gamithasam <gamitha@asia.com>
2024-12-30 16:15:09 +0530

    Bottom button, stepper thing in SelectExercise

--- Trainly/Views/SelectExerciseView.swift
+++ Trainly/Views/SelectExerciseView.swift
@@ -14,32 +14,105 @@ struct SelectExerciseView: View {
         GridItem(.adaptive(minimum: 100, maximum: 200), spacing: 16)
     ]
     @State private var selectedIndex: ExerciseEntity?
+    @State private var sets: Int = 1
+    @State private var reps: Int = 1
+    @State private var duration: Int = 1
+    @State private var showAlert = false
     
     var body: some View {
         NavigationView {
-            GeometryReader { geometry in
-                ScrollView {
-                    LazyVGrid(columns: columns, spacing: 16) {
-                        ForEach(vm.savedExercises) { item in
-                            ExerciseItem(
-                                geometry: geometry,
-                                isSelected: selectedIndex == item,
-                                item: item
-                            )
-                            .onTapGesture {
-                                if selectedIndex == item {
-                                    selectedIndex = nil
-                                } else {
-                                    selectedIndex = item
+            ZStack {
+                GeometryReader { geometry in
+                    ScrollView {
+                        LazyVGrid(columns: columns, spacing: 16) {
+                            ForEach(vm.savedExercises) { item in
+                                ExerciseItem(
+                                    geometry: geometry,
+                                    isSelected: selectedIndex == item,
+                                    item: item
+                                )
+                                .onTapGesture {
+                                    if selectedIndex == item {
+                                        selectedIndex = nil
+                                    } else {
+                                        selectedIndex = item
+                                    }
                                 }
                             }
                         }
+                        .padding()
                     }
-                    .padding()
+                }
+                
+                if selectedIndex != nil {
+                    VStack {
+                        Spacer()
+                        
+                        VStack {
+                            List {
+                                Section {
+                                    HStack {
+                                        Text("Sets:")
+                                        Spacer()
+                                        Stepper(value: $sets, in: 0...100) {
+                                            Text("\(sets)")
+                                        }
+                                    }
+                                }
+                                
+                                Section {
+                                    HStack {
+                                        Text("Duration:")
+                                        Spacer()
+                                        Stepper(value: $duration, in: 0...100) {
+                                            Text("\(duration)")
+                                        }
+                                    }
+                                    HStack {
+                                        Text("Reps:")
+                                        Spacer()
+                                        Stepper(value: $reps, in: 0...100) {
+                                            Text("\(reps)")
+                                        }
+                                    }
+                                }
+                            }
+                            .frame(height: 200)
+                            .padding(.bottom, 34)
+                            .scrollDisabled(true)
+                            .scrollContentBackground(.hidden)
+                            .background(Color.clear)
+                            
+                            Button(action: {
+                                // TODO: add button action
+                                reps = 1
+                                sets = 1
+                                duration = 1
+                                selectedIndex = nil
+                                showAlert = true
+                            }) {
+                                Text("Add")
+                                    .foregroundColor(.white)
+                                    .frame(height: 50)
+                                    .frame(maxWidth: .infinity)
+                                    .background(Color.accentColor)
+                                    .cornerRadius(12)
+                                    .padding(.horizontal)
+                            }
+                        }
+                        .padding(.vertical, 20)
+                        .frame(maxWidth: .infinity)
+                        .background(.ultraThinMaterial)
+                    }
                 }
             }
             .navigationTitle(type)
             .navigationBarTitleDisplayMode(.inline)
+            .alert("Done!", isPresented: $showAlert) {
+                Button("OK", role: .cancel) { }
+            } message: {
+                Text("Exercise added")
+            }
         }
     }
 }
